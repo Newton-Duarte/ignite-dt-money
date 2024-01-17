83d5eb6def5591fe8290d3e0c5a11009bc0247e9
Newton Duarte <newton_@hotmail.com>
2024-01-17 18:34:40 -0300

    feat: add transactions context

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,37 @@
+import { PropsWithChildren, createContext, useEffect, useState } from "react";
+
+export interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+interface TransactionsContextType {
+  transactions: Transaction[]
+}
+
+export const TransactionsContext = createContext({} as TransactionsContextType);
+
+export function TransactionsProvider({ children }: PropsWithChildren) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions');
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
