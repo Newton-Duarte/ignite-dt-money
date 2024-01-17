37dc2e51dde0414384a0234b5871027996b36e97
Newton Duarte <newton_@hotmail.com>
2024-01-17 07:48:30 -0300

    feat: add transactions type components

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
 
 import * as S from './styles';
-import { X } from 'phosphor-react';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 
 export function NewTransactionModal() {
   return (
@@ -18,6 +18,18 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Descrição" required />
           <input type="text" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <S.TransactionTypeContainer>
+            <S.TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </S.TransactionTypeButton>
+
+            <S.TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </S.TransactionTypeButton>
+          </S.TransactionTypeContainer>
 
           <button type="submit">Cadastrar</button>
         </form>
