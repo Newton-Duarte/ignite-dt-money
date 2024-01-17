036aac36183225046b9e2fb5ffd2ea6ba692462a
Newton Duarte <newton_@hotmail.com>
2024-01-17 19:17:51 -0300

    feat: add react hook form to search form component

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,13 +1,37 @@
+import { useForm } from 'react-hook-form';
 import { MagnifyingGlass } from 'phosphor-react';
+import * as z from 'zod';
 
 import * as S from './styles';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useTransactions } from '../../../../hooks/useTransactions';
+
+const searchFormSchema = z.object({
+  query: z.string(),
+});
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-  return (
-    <S.Container>
-      <input type="text" placeholder="Busque por transações" />
+  const { fetchTransactions } = useTransactions();
+  
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema)
+  });
 
-      <button type="submit">
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    <S.Container onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input 
+        type="text" 
+        placeholder="Busque por transações" 
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
