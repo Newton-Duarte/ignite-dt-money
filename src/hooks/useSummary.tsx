0fe4712f3a55dc8b941280a88f51c5505938ccca
Newton Duarte <newton_@hotmail.com>
2024-01-17 21:37:57 -0300

    feat: add use memo hook to summary value

--- src/hooks/useSummary.tsx
+++ src/hooks/useSummary.tsx
@@ -1,26 +1,29 @@
-import { useTransactions } from './useTransactions'
+import { useMemo } from 'react'
+import { useTransactionsList } from './useTransactionsList'
 
 export function useSummary() {
-  const { transactions } = useTransactions()
+  const transactions = useTransactionsList()
 
-  const summary = transactions.reduce(
-    (accumulator, transaction) => {
-      if (transaction.type === 'income') {
-        accumulator.income += transaction.price
-        accumulator.total += transaction.price
-      } else {
-        accumulator.outcome += transaction.price
-        accumulator.total -= transaction.price
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (accumulator, transaction) => {
+        if (transaction.type === 'income') {
+          accumulator.income += transaction.price
+          accumulator.total += transaction.price
+        } else {
+          accumulator.outcome += transaction.price
+          accumulator.total -= transaction.price
+        }
 
-      return accumulator
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return accumulator
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
